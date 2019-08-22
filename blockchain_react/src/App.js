492ed7549d8299557679106d6c30a1a026e1fabb
Max Plank <darkhan.r4industry@gmail.com>
2019-08-22 20:07:29 +0600

    updated App.js

--- blockchain_react/src/App.js
+++ blockchain_react/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Network} />
+          <Route component={Default} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
