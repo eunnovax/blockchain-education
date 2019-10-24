0ce3ba7c562332ab979b500c8729e0bc2ec65511
Max Plank <darkhan.r4industry@gmail.com>
2019-10-24 18:49:01 +0600

    prepared node for heroku deployment

--- blockchain_node/server.js
+++ blockchain_node/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +21,13 @@ connection.once('open', ()=> {
 const consensusRouter = require('./routes/broadcast');
 app.use('/broadcast', consensusRouter);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static( 'client/build' ));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); //relative path
+    });
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
