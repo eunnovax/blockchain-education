e65b3178dfe2357f324506884cb58523374ad1fa
Max Plank <darkhan.r4industry@gmail.com>
2019-10-25 23:55:16 +0600

    valid blockchain and consensus check before update on router

--- blockchain_node/routes/broadcast.js
+++ blockchain_node/routes/broadcast.js
@@ -16,18 +16,8 @@ router.route("/:id").get((req, res) => {
 
 
 router.route('/add').post((req, res) => {
-  validBlock = (block) => {
-    // let regex = /^(0{4})/;
-    // regex.test(block.hash);
-    //consensus algorithm
-    // broadcast the last block of a particular chain
-    // if (
-    // 1. sha256(prevBlock) === block.prevHash
-    // 2. sha256(block) === block.hash
-    // 3. '0000' === regex.test(block.hash))
-    // { add the block to the chain }
-    // else if (1. sha256(prevPrevBlock) === block.prevHash; 2. && 3.)
-    // {add new chain as fork}
+  validChain = (block) => {
+    
   }
     const title = req.body.title;
     const nodes = req.body.nodes;
@@ -72,32 +62,38 @@ router.route("/update/:id").post((req, res) => {
       blockchain.nodes = req.body.nodes;
       blockchain.chainId = req.body.chainId;
       let chain = [...req.body.chain];
-      // req.body.chain.map(block => {
-      //   const blockNumber = block.blockNumber;
-      //   const data = block.data;
-      //   const nonce = block.nonce;
-      //   const previousBlockHash = block.previousBlockHash;
-      //   const timestamp = block.timestamp;
-      //   const hash = block.hash;
-      //   let newBlock = {
-      //       blockNumber: blockNumber,
-      //       data:data,
-      //       nonce: nonce,
-      //       previousBlockHash: previousBlockHash,
-      //       timestamp: timestamp,
-      //       hash: hash
-      //     };
-      //   chain = [...chain, newBlock];  
-      //   // console.log('chain so far', chain);
-      // });
+      //block and its data is extracted 
+      if (chain.length <= 1) {
+        console.log('blockchain is not updated!')
+      } else {
+          for (let i=0; i<chain.length; i++) {
+            const block = req.body.chain[chain.length-1-i];
+            const blockNumber = block.blockNumber;
+            const data = block.data;
+            const nonce = block.nonce;
+            const previousBlockHash = block.previousBlockHash;
+            const timestamp = block.timestamp;
+            const hash = block.hash;
+
+            const prevBlock = req.body.chain[chain.length-2-i];
+            const prevBlockNumber = prevBlock.blockNumber;
+            const prevData = prevBlock.data;
+            const prevNonce = prevBlock.nonce;
+            const prevPreviousBlockHash = prevBlock.previousBlockHash;
+            const prevTimestamp = prevBlock.timestamp;
+            const prevHash = prevBlock.hash;
+      
+      
+          }
+      }
       const block = req.body.chain[chain.length-1];
-      // console.log('block', block);
       const blockNumber = block.blockNumber;
       const data = block.data;
       const nonce = block.nonce;
       const previousBlockHash = block.previousBlockHash;
       const timestamp = block.timestamp;
       const hash = block.hash;
+
       const prevBlock = req.body.chain[chain.length-2];
       let prevBlockNumber;let prevData;let prevNonce;let prevPreviousBlockHash;let prevTimestamp;let prevHash;
       if (prevBlock) {
@@ -108,18 +104,14 @@ router.route("/update/:id").post((req, res) => {
       prevTimestamp = prevBlock.timestamp;
       prevHash = prevBlock.hash;
       };
-      console.log('prevBlock', prevBlock);
-      console.log('prevHash', prevHash);
+
+      //SHA256 of block
       const prevBlockTxt = prevBlockNumber + prevData + prevNonce + prevPreviousBlockHash + prevTimestamp;
       const prevBlockHash = SHA256(prevBlockTxt).toString();
       const blckTxt = blockNumber + data + nonce + previousBlockHash + timestamp;
       const blockHash = SHA256(blckTxt).toString();
-     
+      //block is verified
       const regex = /^(0{4})/;
-      console.log('prevBlockHash', prevBlockHash);
-      console.log('previousBlockHash', previousBlockHash);
-      console.log('blockHash', blockHash);
-      console.log('hash', hash);
       console.log('mined', regex.test(hash));
           // if (prevBlockHash === previousBlockHash && blockHash === hash && regex.test(hash) === true) {
           //   console.log('block is valid');
